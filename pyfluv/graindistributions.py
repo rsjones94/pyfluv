98b2390cfa9485c98e144ed071ad5e477b66a04f
Sky Jones <rsajones94@gmail.com>
2018-12-25 18:47:50 -0600

    Flesh out GrainDistribution class

--- pyfluv/graindistributions.py
+++ pyfluv/graindistributions.py
@@ -45,9 +45,45 @@ class GrainDistribution(object):
         self.name = name
         self.metric = metric
         self.triggerRecalc = triggerRecalc
+        if self.metric:
+            self.unitDict = sc.METRIC_CONSTANTS
+        elif not(self.metric):
+            self.unitDict = sc.IMPERIAL_CONSTANTS
         
         self.calculate_stats()
-           
+        
+    def __str__(self):
+        """
+        Prints the name of the GrainDistribution object. If the name attribute is None, prints "UNNAMED".
+        """
+        if self.name:
+            return(self.name)
+        else:
+            return("UNNAMED")
+            
+    def make_bindict(self):
+        """
+        Makes a dictionary that relates ISO 14688-1:2002 classification names to lower size bounds.
+        """
+        self.bins = {
+                'C':[0], #clay
+                'FM':[0.002], #fine silt
+                'MM':[0.0063], #medium silt
+                'CM':[0.02], #coarse sit
+                'FS':[0.063], #fine sand
+                'MS':[0.2], #medium sand
+                'CS':[0.63], #coarse sand
+                'FG':[2], #fine gravel
+                'MG':[6.3], #medium gavel
+                'CG':[20], # coarse gravel
+                'Cob':[63], #cobble
+                'Bol':[200], #boulder
+                'LBol':[630] #large boulder
+                }
+        if not self.metric:
+            for key in self.bins:
+                self.bins[key] = [self.bins[key][0] * self.unitDict['milToInches']]   
+        
     def reset_distr(self,value,recalculate = None):
         """
         When distr is redefined.
@@ -56,7 +92,7 @@ class GrainDistribution(object):
         if recalculate == None:
             recalculate = self.triggerRecalc
         if recalculate:
-            self.sort_distr()
+            self.calculate_stats()
         
     def get_distr(self):
         """
@@ -71,9 +107,17 @@ class GrainDistribution(object):
         """
         Sort the distr, get the cumsum and calculate statistics.
         """
+        self.make_bindict()
         self.sort_distr()
         self.make_cum_sum()
         self.normalize_cum_sum()
+        self.make_median_grainsize()
+        self.make_mean_grainsize()
+        self.make_sorting()
+        self.make_skewness()
+        self.make_kurtosis()
+        self.make_stddev()
+        self.bin_particles()
     
     def sort_distr(self):
         """
@@ -109,7 +153,6 @@ class GrainDistribution(object):
         
         cSum = [0]
         cSum.extend(self.normCumSum)
-        print(cSum)
         
         for i in range(0,len(cSum)):
             if cSum[i] >= x:
@@ -126,13 +169,133 @@ class GrainDistribution(object):
         dx = sm.x_from_equation(x,regression)
         return(dx)
         
+    def num_to_phi(self,n,metric=None):
+        """
+        Converts a grainsize to its phi (-log2) representation. Takes inches or mm.
+        """
+        if metric is None:
+            metric = self.metric
+        if not metric:
+            n = n * self.unitDict['inchesToMil']
+            
+        phi = -np.log2(n)
+        return(phi)
         
+    def phi_to_num(self,phi,metric=None):
+        """
+        Converts a grainsize from its phi (-log2) representation into inches or mm.
+        """
+        if metric is None:
+            metric = self.metric
+        n = 2**(-phi)
+        if not metric:
+            n = n * self.unitDict['milToInches']
+        return(n)
         
-    def cplot(self):
+    def make_median_grainsize(self):
+        """
+        Calculates the median grainsize of the pebble count.
+        """
+        self.medianSize = self.dx(50)
+        
+    # grainsize stats from http://core.ecu.edu/geology/rigsbyc/rigsby/Sedimentology/CalculationOfGrainSizeStatistics.pdf
+    
+    def make_mean_grainsize(self):
+        """
+        Calculates the mean grainsize of the pebble count in the proper unit (inches or millimeters, not phi).
+        """
+        meanGrainsize = (self.num_to_phi(self.dx(16)) + self.num_to_phi(self.dx(50)) + self.num_to_phi(self.dx(84))) / 3
+        self.meanGrainsize = self.phi_to_num(meanGrainsize)
+        
+    def make_sorting(self):
+        """
+        Calculates the sorting of the pebble count.
+        """
+        sorting = (self.num_to_phi(self.dx(84)) - self.num_to_phi(self.dx(16)))/4 + (self.num_to_phi(self.dx(95)) - self.num_to_phi(self.dx(5)))/6.6
+        self.sorting = sorting
+        
+    def make_skewness(self):
+        """
+        Calculates the skewness of the curve.
+        """
+        skewness = (self.num_to_phi(self.dx(16)) + self.num_to_phi(self.dx(84)) - 2*self.num_to_phi(self.dx(50))) / (2*(self.num_to_phi(self.dx(84)) - self.num_to_phi(self.dx(16))))
+        + (self.num_to_phi(self.dx(5)) + self.num_to_phi(self.dx(95)) - 2*self.num_to_phi(self.dx(50))) / (2*(self.num_to_phi(self.dx(95)) - self.num_to_phi(self.dx(5))))
+        self.skewness = skewness
+        
+    def make_kurtosis(self):
+        """
+        Calculates the kurtosis of the curve.
+        """
+        kurtosis = (self.num_to_phi(self.dx(95)) - self.num_to_phi(self.dx(5)))/(2.44*(self.num_to_phi(self.dx(75)) - self.num_to_phi(self.dx(25))))
+        self.kurtosis = kurtosis
+        
+    def make_stddev(self):
+        """
+        Sets the standard deviation of the pebble count.
+        """
+        self.stddev = (self.dx(84) - self.dx(16)) / 2
+        
+    def make_countlist(self):
+        """
+        Takes the distr dict and returns a list that contains every particle as its own entry in a list.
+        """
+        partlist = []
+        for key in self.distr:
+            partlist.extend([key]*self.distr[key])
+        return(partlist)
+        
+    def bin_particles(self):
+        """
+        Bins the distr into appropriate categories (fine sand, coarse gravel, silt, etc.) and appends it to the appriate self.bins key
+        Should not be called twice before resetting the distr dictionary.
+        """
+        particles = self.make_countlist()
+        partbins = list(self.bins.values())
+        newpartbins = [] # we need to remove the inner lists from partbins
+        for partbin in partbins:
+            newpartbins.append(partbin[0])
+        bincount = np.digitize(particles, newpartbins, right=False) - 1
+        
+        tacklist = [0]*(len(partbins))
+        for i in range(0,len(bincount)):
+            location = bincount[i]
+            tacklist[location] += 1
+            
+        keys = list(self.bins.keys()) 
+        for i in range(len(keys)):
+            key = keys[i]
+            self.bins[key].append(tacklist[i])
+            
+    def estimate_mannings_n(self):
+        """
+        Estimate's Manning's n in the channel based on roughness. Not accurate for vegetated channels.
+        """
+        pass
+    
+    def extract_binned_counts(self):
+        """
+        Returns a list of pebble count values that corresponds to the order of the keys for self.bins
+        """
+        thelist = []
+        for key in self.bins:
+            thelist.append(self.bins[key][1])
+        return(thelist)
+    
+    def extract_binned_cumsum(self):
+        binned = self.extract_binned_counts()
+        return(list(np.cumsum(binned)))
+        
+    def cplot(self,normalize=True):
         """
         A cumulative distribution plot of the particle sizes.
         """
-        pass
+        keys = list(self.bins.keys())
+        vals = self.extract_binned_cumsum()
+        if normalize:
+            vals = np.asarray(vals)
+            factor = 100/max(vals)
+            vals = list(vals*factor)
+        plt.plot(keys,vals)
     
     def dplot(self):
         """
@@ -140,8 +303,14 @@ class GrainDistribution(object):
         """
         pass
     
-    def bplot(self):
+    def bplot(self,normalize=True):
         """
         A bar plot of of the particle sizes.
         """
-        pass
+        keys = list(self.bins.keys())
+        vals = self.extract_binned_counts()
+        if normalize:
+            vals = np.asarray(vals)
+            factor = 100/max(vals)
+            vals = list(vals*factor)
+        plt.bar(keys,vals)
--- pyfluv/test.py
+++ pyfluv/test.py
@@ -6,10 +6,12 @@ import streamgeometry as strgeo
 import graindistributions as grain
 
 
-distr = {2:20,
-         4:30,
-         7:10,
-         5:3
+distr = {2:3,
+         4:1,
+         7:4,
+         5:2,
+         0.01:3,
+         0.4:4
         }
 
 work = grain.GrainDistribution(distr,name ='Work',triggerRecalc=True)
