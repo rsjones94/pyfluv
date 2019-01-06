cf7518e31faaddf1b7ddc73dc2d61d6598438f9f
Sky Jones <rsajones94@gmail.com>
2019-01-06 17:43:21 -0600

    v0.2.0 release update to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pyfluv',
   packages = ['pyfluv'],
-  version = '0.11',
+  version = '0.2.0',
   license='GNU General Public License v3 (GPLv3)',
   description = 'A package for analyzing fluvial planforms, profiles and cross sections, with a focus on restoration.',
   author = 'Sky Jones',
   author_email = 'rsajones94@gmail.com',
   url = 'https://github.com/rsjones94',
-  download_url = 'https://github.com/rsjones94/pyfluv/archive/v_0.11-alpha.tar.gz',
+  download_url = 'https://github.com/rsjones94/pyfluv/archive/v0.2.0.tar.gz',
   keywords = [
               'river', 'fluvial', 'geomorphology', 'stream',
               'analysis', 'profile', 'planform', 'geometry', 'rosgen', 'restoration'
