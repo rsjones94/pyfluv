ec70ca09e597257ec107da5c5381ed9d0c84b633
Sky Jones <rsajones94@gmail.com>
2019-01-06 18:29:37 -0600

    v0.2.1 release

--- pyfluv/__init__.py
+++ pyfluv/__init__.py
@@ -1,3 +1,6 @@
+import pyfluv.streamconstants
+import pyfluv.streammath
+
 from pyfluv.streamgeometry import CrossSection
 from pyfluv.graindistributions import GrainDistribution
 from pyfluv.westpineyriver import crosses
