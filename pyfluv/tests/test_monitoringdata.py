f9eee86de5d36c84eab9ce639e77be3891764d42
Sky.Jones <rsajones94@gmail.com>
2019-05-03 16:37:12 -0500

    Add docstring for test

--- pyfluv/tests/test_monitoringdata.py
+++ pyfluv/tests/test_monitoringdata.py
@@ -6,7 +6,10 @@ from .. import monitoringdata
 
 
 def test_standard_survey():
-
+    """
+    Check to see if we get a Survey object back and if it has all the profiles
+    and cross sections.
+    """
     survey = monitoringdata.standard_survey()
     testPros = ['proTrib2SUP', 'proTrib2', 'proR1', 'proTrib1', 'proR3', 'proR2']
     testCrosses = ['xsTrib2SUPpo', 'xsTrib2SUPri', 'xsTrib2po', 'xsTrib2ri',
