1b2d30d8905568152f28835afa217881a9d16f84
Sky.Jones <rsajones94@gmail.com>
2019-05-07 14:13:34 -0500

    Add more tests for monitoringdata

--- pyfluv/tests/test_monitoringdata.py
+++ pyfluv/tests/test_monitoringdata.py
@@ -1,5 +1,5 @@
 """
-Unit tests for monitoringdata.py using pytest
+Tests for monitoringdata.py using pytest
 """
 
 from .. import monitoringdata
@@ -8,7 +8,7 @@ from .. import monitoringdata
 def test_standard_survey():
     """
     Check to see if we get a Survey object back and if it has all the profiles
-    and cross sections.
+    and cross sections
     """
     survey = monitoringdata.standard_survey()
     testPros = ['proTrib2SUP', 'proTrib2', 'proR1', 'proTrib1', 'proR3', 'proR2']
@@ -20,4 +20,31 @@ def test_standard_survey():
     getCrosses = list(survey.get_names()['Cross Sections'].keys())
 
     assert getPros == testPros and getCrosses == testCrosses
-
+    
+def test_standard_pebbles():
+    """
+    Check to see if we get a list of GraindDistribution objects length 10
+    """
+    
+    checkList = ['Reach 1 XS',
+                 'Reach 2 XS',
+                 'Reach 3 XS',
+                 'Trib1 XS',
+                 'Trib2 XS',
+                 'R1 Profile',
+                 'Reach 2 Profile',
+                 'Reach 3 Profile',
+                 'Trib1 Profile',
+                 'Trib2 Profile']
+    
+    pebs = monitoringdata.standard_pebbles()
+    pebNames = [l.name for l in pebs]
+    
+    assert pebNames == checkList
+    
+def test_eco71():
+    """
+    Test that we have a Reference object for ecoregion 71
+    """
+    curve = monitoringdata.eco71()
+    assert curve.eco == 71
